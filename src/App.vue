cb4f2c6a6fd154c28b879799ccf6a6b6390f361b
胡江 <17600112740@163.com>
2022-03-28 17:29:47 +0800

    test husky -2

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <nav>
     <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
+    <router-link to="/about">About-2</router-link>
   </nav>
   <router-view/>
 </template>
