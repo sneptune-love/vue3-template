51f5b0eab33bebe9101a17807a419ffe387c954b
胡江 <17600112740@163.com>
2022-03-29 09:47:40 +0800

    husky-3

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <nav>
     <router-link to="/">Home</router-link> |
-    <router-link to="/about">About -12</router-link>
+    <router-link to="/about">About -13</router-link>
   </nav>
   <router-view/>
 </template>
